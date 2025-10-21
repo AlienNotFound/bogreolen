a79692ded19dbb4680e1c226856601ca4725cc15
Adam Dreiøe <adr@emd.dk>
2025-10-21 22:28:02 +0200

    attempt at workflow .sql file fix

--- backend/sql/bogreolen_database_bookstb.sql
+++ backend/sql/bogreolen_database_bookstb.sql
@@ -1,14 +1,14 @@
-CREATE DATABASE  IF NOT EXISTS `bogreolen_database`;
-USE `bogreolen_database`;
+CREATE DATABASE  IF NOT EXISTS 'bogreolen_database';
+USE 'bogreolen_database';
 
-DROP TABLE IF EXISTS `bookstb`;
-CREATE TABLE `bookstb` (
-  `bookid` int NOT NULL AUTO_INCREMENT,
-  `title` varchar(30) NOT NULL,
-  `authorid` int NOT NULL,
-  `image` varchar(30) NOT NULL,
-  `summary` varchar(200) NOT NULL,
-  `year` int NOT NULL,
-  `categoryid` int NOT NULL,
-  PRIMARY KEY (`bookid`)
+DROP TABLE IF EXISTS 'bookstb';
+CREATE TABLE 'bookstb' (
+  'bookid' int NOT NULL AUTO_INCREMENT,
+  'title' varchar(30) NOT NULL,
+  'authorid' int NOT NULL,
+  'image' varchar(30) NOT NULL,
+  'summary' varchar(200) NOT NULL,
+  'year' int NOT NULL,
+  'categoryid' int NOT NULL,
+  PRIMARY KEY ('bookid')
 ) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
