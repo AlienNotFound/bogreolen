c62473670b8ee0ee377e1a34813250c6f8f1e918
Adam Dreiøe <adr@emd.dk>
2025-10-21 22:09:53 +0200

    created basic book crud and started updating the pytest

--- backend/tests/basetests.py
+++ backend/tests/basetests.py
@@ -1,10 +1,17 @@
 import pytest
 from backend.connection import db, create_app
 from backend.models.bookstb import Bookstb
+from backend.services.book_service import BookService
+
+book = Bookstb(title="Test book",
+                authorid=1,
+                image="imageurl.jpg",
+                summary="This is a summary",
+                year=2000,
+                categoryid=1)
 
 @pytest.fixture
 def test_db():
-
     app = create_app({
         'SQLALCHEMY_DATABASE_URI': 'sqlite:///:memory:',
         'TESTING': True
@@ -15,15 +22,36 @@ def test_db():
         yield app
         db.drop_all()
 
-def test_book_creation(test_db):
+def test_create_book(test_db):
     with test_db.app_context():
-        book = Bookstb(title="Test book", authorid=1, image="imageurl.jpg", summary="This is a summary", year=2000, categoryid=1)
-        db.session.add(book)
-        db.session.commit()
+        result = BookService.create_book(
+            book.title,
+            book.authorid,
+            book.image,
+            book.summary,
+            book.year,
+            book.categoryid
+        )
 
-        fetched = Bookstb.query.first()
-        assert fetched.title == "Test book"
-        assert fetched.year == 2000
+        print(result)
+
+        # assert fetched.id == 1
+        # assert fetched.title == "Test book"
+        # assert fetched.authorid == 1
+        # assert fetched.image == "imageurl.jpg"
+        # assert fetched.summary == "This is a summary"
+        # assert fetched.year == 2000
+        # assert fetched.categoryid == 1
+        assert result == "Entry succesfully added!"
+
+def test_create_duplicate_fail(test_db):
+    pass
+    # with test_db.app_context():
+    #     db.session.add(book)
+    #     db.session.commit()
+
+
+
 
 def test_book_get_all():
     pass
