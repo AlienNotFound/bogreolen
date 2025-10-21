866b8b1fb3e75a3f8903d25390a1fef6946f6c08
Adam Dreiøe <adr@emd.dk>
2025-10-21 22:17:19 +0200

    updated workflow and added sql files

---
+++ backend/sql/bogreolen_database_routines.sql
@@ -0,0 +1,25 @@
+CREATE DATABASE  IF NOT EXISTS `bogreolen_database`
+USE `bogreolen_database`;
+
+DELIMITER ;;
+CREATE DEFINER=`root`@`%` PROCEDURE `AddBook`(
+    IN p_Title VARCHAR(30),
+    IN p_AuthorID INT, 
+    IN p_Image VARCHAR(30), 
+    IN p_Summary VARCHAR(200), 
+    IN p_Year INT, 
+    IN p_CategoryID INT
+)
+BEGIN
+	IF NOT EXISTS (
+		SELECT 1 FROM bookstb
+		WHERE title = p_Title AND authorid = p_AuthorID
+    ) THEN
+		INSERT IGNORE INTO bookstb (title, authorid, image, summary, year, categoryid)
+		VALUES (p_Title, p_AuthorID, p_Image, p_Summary, p_Year, p_CategoryID);
+        SELECT "Entry succesfully added!" as OUTPUT;
+	ELSE
+		SELECT "Entry already exists!" as OUTPUT;
+	END IF;
+END ;;
+DELIMITER ;
