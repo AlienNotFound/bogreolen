9131c254f6758f40d7181a2ce2db132aa92f5d81
Adam Dreiøe <adr@emd.dk>
2025-10-23 12:48:22 +0200

    added type checking to remove warnings

--- backend/models/authors_model.py
+++ backend/models/authors_model.py
@@ -1,6 +1,10 @@
 from backend.connection import db
 from sqlalchemy import String, UniqueConstraint
 from sqlalchemy.orm import Mapped, mapped_column, relationship
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from .bookstb import Bookstb
 
 class Authorstb(db.Model):
     authorid: Mapped[int] = mapped_column(primary_key=True)
