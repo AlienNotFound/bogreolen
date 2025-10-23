27241cecad06fe0dc11c18f05bc1a124a0be6ce0
Adam Dreiøe <adr@emd.dk>
2025-10-23 23:16:53 +0200

    fixed model import

--- backend/models/user_model.py
+++ backend/models/user_model.py
@@ -4,7 +4,7 @@ from sqlalchemy.orm import Mapped, mapped_column, relationship
 from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
-    from .bookstb import Bookstb
+    from .reviews_model import Reviewstb
 
 class Userstb(db.Model):
     userid: Mapped[int] = mapped_column(primary_key=True)
