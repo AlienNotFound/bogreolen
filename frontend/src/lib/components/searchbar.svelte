a45e2d46538b081abd7c72bc5489df1745ed4b01
Adam Dreiøe <adr@emd.dk>
2025-10-28 20:58:55 +0100

    created searchbar component

--- frontend/src/lib/components/searchbar.svelte
+++ frontend/src/lib/components/searchbar.svelte
@@ -1,4 +1,53 @@
+<script lang="ts">
+    type Book = {
+        title: string
+    }
+
+    let searchInput = $state<string>();
+    let searchSuggestions = $state<Book[]>([]);
+    let showSuggestions = $state(false);
+
+    async function fetchSearch(query: string) {
+        try {
+            if (query) {
+                const response = await fetch('http://localhost:8000/search/' + searchInput);
+                const result = await response.json()
+                searchSuggestions = result;
+                if (result) {
+                    showSuggestions = true;
+                }
+                console.log(result)
+            }
+        } catch (err) {
+            console.error(err)
+        }
+    }
+</script>
 <div>
-    <input type="text">
+    <input type="text"
+            onkeyup={
+                async (e) => {
+                    searchInput = e.currentTarget.value;
+                    fetchSearch(searchInput);
+                    if (e.currentTarget.value == "") {
+                        showSuggestions = false;
+                    }
+                }
+            }
+            >
     <button>Search</button>
+    {#if showSuggestions}
+        <div id="searchSuggestions">
+            {#each searchSuggestions as suggestion}
+                <a href="/">{suggestion.title}</a>
+            {/each}
+        </div>
+    {/if}
 </div>
+
+<style>
+    #searchSuggestions {
+        display: flex;
+        flex-direction: column;
+    }
+</style>
