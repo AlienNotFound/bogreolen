fe1067656fd835997432bf1c235433c9e4dcd10e
Adam <adamdreioe@gmail.com>
2025-11-11 12:03:43 +0100

    updated connection string

--- backend/connection.py
+++ backend/connection.py
@@ -10,13 +10,14 @@ load_dotenv()
 
 MYSQL_HOST = os.getenv('MYSQL_HOST', 'db')
 MYSQL_ROOT_PASSWORD = os.getenv('MYSQL_ROOT_PASSWORD')
-MYSQL_DATABASE= os.getenv('MYSQL_DATABASE')
+MYSQL_DATABASE = os.getenv('MYSQL_DATABASE')
+MYSQL_USER = os.getenv('MYSQL_USER')
 
 db = SQLAlchemy()
 
 def create_app(test_config = None):
     app = Flask(__name__)
-    app.config["SQLALCHEMY_DATABASE_URI"] = 'mysql+pymysql://root:' + MYSQL_ROOT_PASSWORD + '@' + MYSQL_HOST + ':3306/' + MYSQL_DATABASE
+    app.config["SQLALCHEMY_DATABASE_URI"] = 'mysql+pymysql://' + MYSQL_USER + ':' + MYSQL_ROOT_PASSWORD + '@' + MYSQL_HOST + ':3306/' + MYSQL_DATABASE
 
 
     if test_config:
@@ -53,9 +54,9 @@ def create_app(test_config = None):
 
     JWTManager(app)
 
-    CORS(app, origins=['http://localhost:5173', 'http://127.0.0.1:5173'],
+    CORS(app, origins=['http://localhost:5173', 'http://127.0.0.1:5173', 'http://localhost:4173', 'http://127.0.0.1:4173'],
      supports_credentials=True,
      allow_headers=['Content-Type'],
-     methods=['GET', 'POST', 'DELETE', 'PUT'])
+     methods=['GET', 'POST', 'DELETE', 'OPTIONS', 'PUT'])
 
     return app
